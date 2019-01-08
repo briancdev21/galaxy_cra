c0a9865c464220fdbc3ad1c23203ae6a1be4511f
toptal <toptal@toptals-iMac.local>
2019-01-08 14:35:57 -0500

    PingfangSC component responsive

--- src/components/PingFangSC.js
+++ src/components/PingFangSC.js
@@ -5,6 +5,10 @@ const PingFangSC = styled.span`
   font-weight: ${props => props.weight};
   font-size: ${props => props.size}px;
   color: ${props => props.color};
+
+  @media (max-width: 560px) {
+    font-size: ${props => props.mobileSize ? props.mobileSize : props.size * 0.7}px;
+  }
 `;
 
 export default PingFangSC;
