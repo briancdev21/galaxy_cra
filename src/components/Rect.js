3718cc1ac783c732a071b6f4d5be7169933cee07
toptal <toptal@toptals-iMac.local>
2019-01-08 14:35:42 -0500

    Add code for rect responsiveness

--- src/components/Rect.js
+++ src/components/Rect.js
@@ -5,6 +5,14 @@ const Rect = styled.div`
   width: ${props => props.width}px;
   height: ${props => props.width}px;
   background-color: ${props => props.color};
+
+  @media (max-width: 460px) {
+    display: none;
+  }
+  @media (max-width: 560px) {
+    width: ${props => props.width * 0.8}px;
+    height: ${props => props.width * 0.8}px;
+  }
 `;
 
 export default Rect;
