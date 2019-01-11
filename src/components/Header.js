45284642f85c0d33ab9681121408be39f6fb87e1
toptal <toptal@toptals-iMac.local>
2019-01-10 22:44:46 -0500

    Update UX design index page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,71 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Row, Col, Icon, Badge } from 'antd';
 import PingFangSC from './PingFangSC';
 import styled from 'styled-components';
 
 import './Header.css';
+import SecondMenu from './SecondMenu';
 import logo from '../assets/images/LOGO.png';
 
-const Header = () => (
-  <React.Fragment>
-    <div className="container">
-      <Row type="flex" justify="space-between" align="middle" className="header">
-        <Col>
-          <Row type="flex" justify="space-between" align="middle">
-            <img src={logo} width={50} alt="" />
-            &nbsp;&nbsp;
-            <PingFangSC size={24} mobileSize={22} weight={700} color="#4a4a4a">启儿宝</PingFangSC>
-            &nbsp;&nbsp;
-            <PingFangSC size={20} mobileSize={18} weight={700} color="#04b4d4">家庭工作坊</PingFangSC>
+class Header extends PureComponent {
+  state = {
+    showSecondMenu: false,
+    menuOpened: false,
+  }
+
+  onShowSecondMenu = () => {
+    const { showSecondMenu } =  this.state;
+    this.setState({ showSecondMenu: !showSecondMenu, menuOpened: true });
+  }
+
+  onHideMenus = () => {
+    this.setState({ showSecondMenu: false, menuOpened: false });
+  }
+
+  render() {
+    const { showSecondMenu, menuOpened } = this.state;
+    return (
+      <React.Fragment>
+        <MenuWrapper hidden={!menuOpened} onClick={this.onHideMenus} />
+        <div className="container">
+          <Row type="flex" justify="space-between" align="middle" className="header">
+            <Col>
+              <Row type="flex" justify="space-between" align="middle">
+                <img src={logo} width={50} alt="" />
+                &nbsp;&nbsp;
+                <PingFangSC size={24} mobileSize={22} weight={700} color="#4a4a4a">启儿宝</PingFangSC>
+                &nbsp;&nbsp;
+                <PingFangSC size={20} mobileSize={18} weight={700} color="#04b4d4">家庭工作坊</PingFangSC>
+              </Row>
+            </Col>
+            <Col className="icons-list">
+              <Row type="flex" align="middle">
+                <MenuIconWrapper>
+                  <Badge count={2} style={{ backgroundColor: '#faad14' }}>
+                    <Icon type="file-text" style={{ fontSize: '1.5em', color: '#fff' }} />
+                  </Badge>
+                </MenuIconWrapper>
+                <div>
+                  <MenuIconWrapper onClick={this.onShowSecondMenu}>
+                    <Icon type="bars" style={{ fontSize: '1.5em', color: '#fff' }} />
+                  </MenuIconWrapper>
+                  <SecondMenu hidden={!showSecondMenu} onMenuItemClick={this.onHideMenus} />
+                </div>
+                <MenuIconWrapper>
+                  <Icon type="file-text" style={{ fontSize: '1.5em', color: '#fff' }} />
+                </MenuIconWrapper>
+                <MenuIconWrapper>
+                  <Icon type="file-text" style={{ fontSize: '1.5em', color: '#fff' }} />
+                </MenuIconWrapper>
+              </Row>
+            </Col>
           </Row>
-        </Col>
-        <Col className="icons-list">
-          <Row type="flex" align="middle">
-            <MenuIconWrapper>
-              <Badge count={2} style={{ backgroundColor: '#faad14' }}>
-                <Icon type="file-text" style={{ fontSize: '1.5em', color: '#fff' }} />
-              </Badge>
-            </MenuIconWrapper>
-            <MenuIconWrapper>
-              <Icon type="bars" style={{ fontSize: '1.5em', color: '#fff' }} />
-            </MenuIconWrapper>
-            <MenuIconWrapper>
-              <Icon type="file-text" style={{ fontSize: '1.5em', color: '#fff' }} />
-            </MenuIconWrapper>
-            <MenuIconWrapper>
-              <Icon type="file-text" style={{ fontSize: '1.5em', color: '#fff' }} />
-            </MenuIconWrapper>
-          </Row>
-        </Col>
-      </Row>
-    </div>
-  </React.Fragment>
-)
+          
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Header;
 
@@ -59,4 +84,15 @@ const MenuIconWrapper = styled.div`
   &:hover { 
     background-color: #b1b1b1;
   }
+`;
+
+const MenuWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.1);
+  display: ${props => props.hidden ? 'none': 'block'};
+  z-index: 999;
 `;
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as PingFangSC } from './PingFangSC';
 export { default as OpenBtn } from './OpenBtn';
 export { default as RoundRectBtn } from './RoundRectBtn';
 export { default as Progress } from './Progress';
+export { default as SecondMenu } from './SecondMenu';
